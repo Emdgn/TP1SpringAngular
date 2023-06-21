6c22a7a184bab306b4b57c0f891a888854884ecd
Titou82Bi <inacio.baptiste@gmail.com>
2023-06-21 14:06:54 +0200

    test branche

--- src/main/java/com/inti/model/ChefOrchestre.java
+++ src/main/java/com/inti/model/ChefOrchestre.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+// modification test
 @Entity
 @Table
 @Data @NoArgsConstructor @AllArgsConstructor
