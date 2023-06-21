73721c27ad145342fc381c7ec63a6230bb8f33e2
sarahborkan <sarah.behaz1@gmail.com>
2023-06-21 15:40:42 +0200

    correction conflit

--- src/main/java/com/inti/model/Concert.java
+++ src/main/java/com/inti/model/Concert.java
@@ -1,11 +1,15 @@
 package com.inti.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,22 +17,37 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table
-@Data @AllArgsConstructor @NoArgsConstructor
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Concert {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int num;
 	private String nom;
 	private LocalDate date;
+
+
+	
+	@OneToMany(mappedBy = "concert")
+	private List<Oeuvre> listeO;
+	
+
+	@OneToOne
+	@JoinColumn(name = "lieu_id")
+	private Lieu lieu;
+
 	
 	public Concert(String nom, LocalDate date) {
 		super();
 		this.nom = nom;
 		this.date = date;
+
+
+
 	}
-	
-	
-	
-	
+
 }
