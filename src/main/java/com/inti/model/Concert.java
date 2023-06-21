be6d3e63d92e658877cd304e965978d047ef55f1
Titou82Bi <inacio.baptiste@gmail.com>
2023-06-21 13:51:01 +0200

    Merge branch 'master' of https://github.com/landazar/TP1Spring.git

--- src/main/java/com/inti/model/Concert.java
+++ src/main/java/com/inti/model/Concert.java
@@ -7,12 +7,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-<<<<<<< HEAD
-import jakarta.persistence.OneToMany;
-=======
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
->>>>>>> branch 'master' of https://github.com/landazar/TP1Spring.git
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -32,27 +29,22 @@ public class Concert {
 	private int num;
 	private String nom;
 	private LocalDate date;
-	
-<<<<<<< HEAD
+
 	
 	@OneToMany(mappedBy = "concert")
 	private List<Oeuvre> listeO;
 	
-=======
+
 	@OneToOne
 	@JoinColumn(name = "lieu_id")
 	private Lieu lieu;
->>>>>>> branch 'master' of https://github.com/landazar/TP1Spring.git
+
 	
 	public Concert(String nom, LocalDate date) {
 		super();
 		this.nom = nom;
 		this.date = date;
-<<<<<<< HEAD
 
 	}
-	
-=======
-	}
->>>>>>> branch 'master' of https://github.com/landazar/TP1Spring.git
+
 }
