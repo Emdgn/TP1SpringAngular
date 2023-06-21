9182ddb8df268b82b6aa23ee6bd85143eae50548
landazar <lucaspagies@gmail.com>
2023-06-21 13:53:48 +0200

    enlever import en trop

--- src/main/java/com/inti/model/ChefOrchestre.java
+++ src/main/java/com/inti/model/ChefOrchestre.java
@@ -7,18 +7,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+
 
 @Entity
 @Table
