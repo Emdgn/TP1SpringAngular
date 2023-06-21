a21aae0148a8831469f882f3f02c82ccae0ab459
sarahborkan <sarah.behaz1@gmail.com>
2023-06-21 15:01:59 +0200

    commit to pull

--- src/main/java/com/inti/model/Concert.java
+++ src/main/java/com/inti/model/Concert.java
@@ -1,17 +1,33 @@
-package com.inti;
+package com.inti.model;
 
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
+@Data @AllArgsConstructor @NoArgsConstructor
 public class Concert {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int num;
 	private String nom;
 	private LocalDate date;
+	
+	public Concert(String nom, LocalDate date) {
+		super();
+		this.nom = nom;
+		this.date = date;
+	}
+	
 	
 	
 	
