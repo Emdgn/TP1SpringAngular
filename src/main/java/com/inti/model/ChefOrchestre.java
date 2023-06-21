51fd60b4e6509929b292720c5a875c5ac2733a06
Titou82Bi <inacio.baptiste@gmail.com>
2023-06-21 13:54:00 +0200

    gestion des import inutiles

--- src/main/java/com/inti/model/ChefOrchestre.java
+++ src/main/java/com/inti/model/ChefOrchestre.java
@@ -1,24 +1,15 @@
 package com.inti.model;
-
 import java.time.LocalDate;
 import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Table
