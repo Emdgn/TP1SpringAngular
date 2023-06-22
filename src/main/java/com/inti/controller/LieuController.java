3d4bb7fc1dbcff254cddb25cdc53054abef8790e
sarahborkan <sarah.behaz1@gmail.com>
2023-06-22 09:36:18 +0200

    ajout controller et interface

--- src/main/java/com/inti/controller/LieuController.java
+++ src/main/java/com/inti/controller/LieuController.java
@@ -17,21 +17,21 @@ public class LieuController {
     @Autowired
     private ILieuRepository ilr;
 
-    @GetMapping("/lieux")
+    @GetMapping("lieux")
     public String getAllLieux(Model model) {
         List<Lieu> lieux = ilr.findAll();
         model.addAttribute("lieux", lieux);
         return "listeLieux";
     }
 
-    @GetMapping("/lieux/ajouter")
+    @GetMapping("ajouterLieu")
     public String addLieu(Model model) {
         Lieu lieu = new Lieu();
         model.addAttribute("lieu", lieu);
         return "ajoutLieu";
     }
 
-    @PostMapping("/lieux/ajouter")
+    @PostMapping("ajouterLieu")
     public String addLieu(@ModelAttribute("lieu") Lieu lieu) {
         ilr.save(lieu);
         return "redirect:/lieux";
